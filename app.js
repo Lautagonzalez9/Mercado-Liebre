00c327ae4e464e0ced4e2f56fff8b200a305d189
Lautagonzalez9 <lauta_gonzalez29@hotmail.com>
2023-01-04 21:03:25 -0300

    agregando el render

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const publicPath= path.join(__dirname,'/public');
 app.use( express.static(publicPath) )
 //Levantar Servidor
 const port = process.env.PORT || 3000;
-app.listen(port,() =>console.log('servidor corriendo en el puerto$(port)'));
+app.listen(port,() =>console.log('servidor corriendo en el puerto $(port)'));
 //Rutas & links.
 app.get('localhost:3000/',(req,res)=>{res.sendFile(path.join(__dirname,'./views/home.html'))});
 app.get('/',(req,res)=>{res.sendFile(path.join(__dirname,'./views/home.html'))});
@@ -19,6 +19,6 @@ app.get('/login',(req,res)=>{res.sendFile(path.join(__dirname,'./views/login.htm
 
 
 Scripts:{
-    start:"node app.js"
-    test:"nodemon app.js"
+    start:"node src/app.js"
+    test:"nodemon src/app.js"
 }
